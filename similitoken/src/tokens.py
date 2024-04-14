1e37a256443331ac9fe1040c362aa16442917cdd
aleubeto <aleubeto.dev@gmail.com>
2024-04-13 21:31:55 -0600

    feat(tokens): :sparkles: Create python tokens parser
    Completed function that generates pythons functions by passing the files data and using the python parser library classes.

--- similitoken/src/tokens.py
+++ similitoken/src/tokens.py
@@ -1,6 +1,16 @@
-def generate_python_tokens(file_content: str) -> list:
-    """"""
-    return []
+from pythonparser import lexer, source, diagnostic
+from typing import Dict
+
+
+def generate_python_tokens(file_data: Dict[str, str]) -> list:
+    """Return a list of Token objects with the tokens found in a code file."""
+    file_path = file_data.get("path")
+    file_content = file_data.get("content")
+    buffer = source.Buffer(file_content, file_path)
+    engine = diagnostic.Engine()
+    tokens = lexer.Lexer(buffer, (3, 6), diagnostic_engine=engine)
+    tokens_list = [token for token in tokens]
+    return tokens_list
 
 
 token_functions = {
