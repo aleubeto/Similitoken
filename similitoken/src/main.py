e98461ccc0a34d629aa0681d5acd9d887dd9a26f
aleubeto <aleubeto.dev@gmail.com>
2024-04-14 10:56:41 -0600

    chore(tokens): :construction: Create main script to test tokens functionality
    Created basic main script to test tokenize feature.

--- similitoken/src/main.py
+++ similitoken/src/main.py
@@ -1,2 +1,11 @@
+from file_manager import FileManager
+
 if __name__ == "__main__":
-    print("Hello world!")
+    files = FileManager()
+    file1 = "similitoken/tests/input_files/sum_1.py"
+    file2 = "similitoken/tests/input_files/sum_2.py"
+
+    files.load_file(file1)
+    files.load_file(file2)
+
+    files.compare_files(file1, file2)
