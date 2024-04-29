6094d82eb3e9861ee8f9a02c3ecce1592b658b92
aleubeto <aleubeto.dev@gmail.com>
2024-04-29 08:27:43 -0600

    docs(setup): :memo: Include authors, licence and url info
    Included authors names and emails; Specified MIT license and created LICENSE file; Provided url of the repository.

--- similitoken/setup.py
+++ similitoken/setup.py
@@ -2,11 +2,26 @@ from setuptools import setup, find_packages
 
 setup(
     name="similitoken",
-    author="Alejandro Ubeto",
     version="0.1",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "pythonparser @ https://github.com/m-labs/pythonparser/archive/master.zip",
-        "matplotlib"
+        "matplotlib",
     ],
+    author=[
+        "Alejandro Alfonso Ubeto Yañez",
+        "Óscar Emiliano Ramírez Díaz",
+        "Iker Guerrero González",
+        "Ángel Rubén Vázquez Rivera",
+        "Francisco Rocha Juárez",
+    ],
+    author_email=[
+        "aleubeto.dev@gmail.com",
+        "oscardiaz.dev@gmail.com",
+        "ikerguerrero@yahoo.com",
+        "developerrv1024@gmail.com",
+        "rochapakito@gmail.com",
+    ],
+    license="MIT",
+    url="https://github.com/aleubeto/Similitoken",
 )
